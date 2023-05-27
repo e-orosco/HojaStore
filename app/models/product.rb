b24d1a5a23d8267f19f9f9052f9871cb40ae3723
Esteban Orosco <eorosco360@gmail.com>
2023-05-27 17:51:50 -0500

    Upgrade and design  product cards

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :seller
-
+  has_many_attached :images
+  
   validates :title, :price, presence: true
   validates :description, presence: true, length: { maximum: 25 }
+  
 end
--- app/models/seller.rb
+++ app/models/seller.rb
@@ -4,6 +4,6 @@ class Seller < ApplicationRecord
 	has_secure_password
 	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
 	validates :email, presence: true, uniqueness: true
-	
-	
+
+	 
 end
